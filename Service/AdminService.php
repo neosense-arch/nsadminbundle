1c5fab30bfb2c256458dccf30f4249299fc0a971
Alexey Gromov <italianwind@gmail.com>
2013-02-22 00:45:49 +0400

    NEW Added getActiveBundles method for admin service

--- Service/AdminService.php
+++ Service/AdminService.php
@@ -2,11 +2,14 @@
 
 namespace NS\AdminBundle\Service;
 
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Kernel;
+
 /**
  * Admin service
  *
  */
-class AdminService
+final class AdminService
 {
 	/**
 	 * @var string[]
@@ -14,11 +17,18 @@ class AdminService
 	private $bundles;
 
 	/**
-	 * @param string[] $bundles
+	 * @var Kernel
 	 */
-	public function __construct(array $bundles)
+	private $kernel;
+
+	/**
+	 * @param string[] $bundles
+	 * @param Kernel   $kernel
+	 */
+	public function __construct(array $bundles, Kernel $kernel)
 	{
 		$this->bundles = $bundles;
+		$this->kernel  = $kernel;
 	}
 
 	/**
@@ -30,5 +40,19 @@ class AdminService
 	public function isBundleActive($name)
 	{
 		return in_array($name, $this->bundles);
+	}
+
+	/**
+	 * Retrieves active bundles
+	 *
+	 * @return Bundle[]
+	 */
+	public function getActiveBundles()
+	{
+		$bundles = $this->bundles;
+
+		return array_filter($this->kernel->getBundles(), function(Bundle $bundle) use($bundles){
+			return in_array($bundle->getName(), $bundles);
+		});
 	}
 }
