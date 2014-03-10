56e067a5a9e4b236f8fc26875499b75e70c6ab25
Alexey Gromov <italianwind@gmail.com>
2014-03-10 18:20:22 +0400

    NEW Added system info tab

--- Controller/AdminToolsController.php
+++ Controller/AdminToolsController.php
@@ -2,6 +2,7 @@
 
 namespace NS\AdminBundle\Controller;
 
+use NS\CoreBundle\Service\VersionService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Knp\Menu\ItemInterface;
@@ -14,15 +15,20 @@ class AdminToolsController extends Controller
 	 */
 	public function phpinfoAction()
 	{
+        // cms info
+        /** @var VersionService $versionService */
+        $versionService = $this->get('ns_core.service.version');
+
+        // phpinfo
         ob_start();
         phpinfo();
         $phpInfo = ob_get_contents();
         ob_end_clean();
-
         $phpInfo = preg_replace( '%^.*<body>(.*)</body>.*$%ms','$1',$phpInfo);
 
 		return $this->render('NSAdminBundle:AdminTools:phpinfo.html.twig', array(
-            'phpInfo' => $phpInfo,
+            'phpInfo'    => $phpInfo,
+            'cmsVersion' => $versionService->getVersion(),
         ));
 	}
 }
